ff13b0ce01d29bc133e99d4480d745d418a997bb
Chibuzo <chibuzo.henry@gmail.com>
2023-05-17 05:48:52 +0100

    Fix typo

--- services/internshipService.js
+++ services/internshipService.js
@@ -21,7 +21,7 @@ const apply = async (internship_id, student_id) => {
 
 const listApplications = async () => {
     const [data] = await sequelize.query(`SELECT CONCAT(u.first_name, ' ', u.last_name) student_name, ia.date, i.title, i.start_date, i.end_date, o.name organization, o.industry
-        FROM internshipapplications ia 
+        FROM internshipApplications ia 
         LEFT JOIN students u ON u.id = ia.student_id
         LEFT JOIN internships i ON i.id = ia.internship_id
         LEFT JOIN organizations o ON o.id = i.organization_id`);
